dc391052aa8e27c78eb8501ff576f08716062ee3
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2024-02-19 20:16:11 +0900

    my 6th commit

--- script.js
+++ script.js
@@ -6,26 +6,49 @@ const apiUrls = [
     `https://newsapijerrykim.netlify.app`,
   ];
 
+  const menus = document.querySelectorAll(".menus button");
+  console.log(menus);
+  menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
+
 const getLatestNews = async () => {
-  const url = new URL(apiUrls[1]);
+  const url = new URL(apiUrls[2]);
 
   const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles;
-  render();
+  render()
   console.log(newsList);
 };
+
+const getNewsByCategory = async (event)=> {
+const category = event.target.textContent.toLowerCase();
+console.log("category", category);
+const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${api_key}`);
+
+const response = await fetch(url);
+const data = await response.json();
+console.log('data',data);
+
+}
+
+
 
 const render = () => {
   let newsHTML = "";
   newsList.map((news) => {
     newsHTML += `
         <div class="news">
+        <div>
+
             <img src="${news.urlToImage}" alt="news-image">
+            </div>
+            <div>
+
             <h2>${news.title}</h2>
             <p>${news.description}</p>
             <div>${news.source.name}+${news.publishedAt}</div>
             <a href="${news.url}" target="_blank">Read More</a>
+            </div>
         </div>
         `;
     return newsHTML;
