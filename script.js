40af527ca8231c3ca09f03475be78ef04545f9c3
ksyjerry <67769150+ksyjerry@users.noreply.github.com>
2024-02-19 13:29:37 +0900

    my second commit

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-const api_key = '1eb650d9dd204edba0e8a861f2f158f4';
-let newsList =[];
+const api_key = "1eb650d9dd204edba0e8a861f2f158f4";
+let newsList = [];
 const APIurl1 = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${api_key}`;
 const APIurl2 = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
-
+const APIurl3 = `https://newsapijerrykim.netlify.app`;
 
 const getLatestNews = async () => {
-    const url = new URL(APIurl2);
+  const url = new URL(APIurl3);
 
-    
-    const response = await fetch(url);
-    const data = await response.json();
-    newsList = data.articles;
-    render();
-    console.log(newsList);
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log(newsList);
 };
 
 const render = () => {
-    let newsHTML = '';
-    newsList.map((news) => {
-        newsHTML += `
+  let newsHTML = "";
+  newsList.map((news) => {
+    newsHTML += `
         <div class="news">
             <img src="${news.urlToImage}" alt="news-image">
             <h2>${news.title}</h2>
@@ -27,13 +26,11 @@ const render = () => {
             <a href="${news.url}" target="_blank">Read More</a>
         </div>
         `;
-        return newsHTML;
+    return newsHTML;
+  });
 
-    });
-    
-    document.getElementById('news-board').innerHTML = newsHTML;
-}
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 
 getLatestNews();
 // render();
-
